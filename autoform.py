9236b184417339bde3b052f72151929eedc04d78
sAksham-Ar <57193459+sAksham-Ar@users.noreply.github.com>
2020-11-05 13:15:25 +0530

    Update autoform.py

--- autoform.py
+++ autoform.py
@@ -2,18 +2,10 @@ import urllib,urllib.request
 from datetime import date
 import random
 today = date.today()
-data = "" #Text you have to fill in the form. You can also read a file and fill its content in a google form.
 num=random.randint(5,10)
 url="https://docs.google.com/forms/d/e/XXXXXXXXXXXXXXXXX/formResponse"
- #URL to the form you want to fill. FormResponse should be used instead of ViewForm
-urlout="https://docs.google.com/forms/d/e/1FAIpQLSdNN3reQre0R6BL4CchFjm20SmujvXPDN15M_rBPwhe1ZkUhw/viewform?"
-text_entry_field={'entry.1489767559':num,'emailAddress':"",'entry.271596398_year':today.year,'entry.271596398_month':today.month,'entry.271596398_day':today.day,'entry.868228904':"",'entry.1867348459':"",'entry.1405267709':""} #Specify the Field Name here
-
-"""
-Hop to your form page, and click Responses > Get pre-filled URL. It'll ask you to do a sample fill-out of your form. I'd suggest putting in responses that easily let you see what question you're answering. Once you hit submit, it's going to give you a URL in the form of...
-https://docs.google.com/forms/d/<form_id>/viewform?entry.XXXXXXX=____&entry.YYYYYY=____...
-Now, change viewform to formResponse within the URL.
-"""
+urlout="https://docs.google.com/forms/d/e/XXXXXXXXXXXXXXXXX/viewform?"
+text_entry_field={'entry.1489767559':num,'emailAddress':"",'entry.271596398_year':today.year,'entry.271596398_month':today.month,'entry.271596398_day':today.day,'entry.868228904':"",'entry.1867348459':"",'entry.1405267709':""}
 
 try:
 	dataenc=urllib.parse.urlencode(text_entry_field).encode('utf_8')
